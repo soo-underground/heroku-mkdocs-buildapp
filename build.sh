6c61bd6d36a004bf679ce031d966f966836cc4d7
soo-underground <bordovskiy92@gmail.com>
2018-07-29 17:13:35 +0300

    fix1

--- build.sh
+++ build.sh
@@ -1,10 +1,19 @@
 cd site
 ls
 git init
+echo "1"
 git add .
+echo "2"
 git config user.email "bordovskiy92@gmail.com"
 git config user.name "soo-underground"
 git commit -m "commit from aws codebuild"
-git remote add origin https://soo-underground:S2shk442@github.com/soo-underground/soo-underground.github.io.git
+echo "3"
+git remote add herokuautobild https://soo-underground:S2shk442@github.com/soo-underground/soo-underground.github.io.git
+echo "4"
 git remote -v
-git push --force origin master
+echo "5"
+git push --force herokutautobuild master
+
+
+
+
