9534e57c3ca5122fb6620c9a9b0c35f1aca0723a
Kirill Bordovskiy <bordovskiy@ld.yandex.ru>
2018-08-01 12:16:00 +0300

    path fix

--- server.py
+++ server.py
@@ -39,7 +39,7 @@ def build_test():
     """
 #    payload = parse_request(request)
     print ('hello world')
-    subprocess.call(['build.sh'], shell=True)
+    subprocess.call(['../../app/build.sh'], shell=True)
     return ("hello world", 200, None)    
 
 if __name__ == '__main__':
