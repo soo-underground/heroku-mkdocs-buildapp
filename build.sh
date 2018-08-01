6b6d0d43dcb37eaabd5f14bbcde1dbae50e6760b
Kirill Bordovskiy <bordovskiy@ld.yandex.ru>
2018-08-01 12:34:17 +0300

    path fix

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ git config user.email "bordovskiy92@gmail.com"
 git config user.name "soo-underground"
 git commit -m "commit from aws codebuild"
 echo "3"
-git remote add herokuautobild https://soo-underground:S2shk442@github.com/soo-underground/soo-underground.github.io.git
+git remote add herokuautobild https://soo-underground:S22@github.com/soo-underground/soo-underground.github.io.git
 echo "4"
 git remote -v
 echo "5"
