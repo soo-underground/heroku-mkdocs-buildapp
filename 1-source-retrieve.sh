1ae5c07d6ba3f391da587fb004c61f1fc9512094
soo-underground <bordovskiy92@gmail.com>
2018-10-25 11:18:57 +0300

    fix?

--- 1-source-retrieve.sh
+++ 1-source-retrieve.sh
@@ -1,6 +1,7 @@
 echo "--------------------------------stage 1: cloning---------------"
-rm docpage-source
+ls
 git clone https://github.com/soo-underground/docpage-source.git
+ls
 #cd my-project
 
 
